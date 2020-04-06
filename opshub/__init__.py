ea4230b394fdd16fd0714a1e24951be4de8e25e8
Henry Parker <henryparker@pop-os.localdomain>
2020-04-06 00:58:10 -0500

    prepare basic logic for sqlalchemy integration

--- opshub/__init__.py
+++ opshub/__init__.py
@@ -5,20 +5,26 @@ OpsHub
 from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_accept import accept
+from opshub import models
 
 version = '0.0.1'
 
 #endpoint used for uploading, fetching, and deleting files.
 class FileEndpoint(Resource):
     def get(self):
-        #TODO:: differentiate endpoint based on passed args, grab data, respond
+        #TODO:: grab data, respond
         return [ { "filename" : "Fake.txt", "date": "now", "id":1 } ]
     
     @accept('multipart/form-data')
     def post(self):
-        f = request.files['file']
-        #TODO:: save file to db, handle errors, return id
-        return { "message": "hello" }
+        files = request.files['file']
+        if len(files) < 1:
+            return { "errors": "file upload failed" }
+        else: #TODO:: show error if multiple files
+            file = files[0]
+            #TODO:: save the file in on the server, return any failures
+            return { "result": file.name }
+
     
     def delete(self):
         #TODO:: grab data, delete file, handle errors, return response
